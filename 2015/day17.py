fd6749163411c6d06c7f7362590719e72d25a5bd
Erik Johnson <palehose@gmail.com>
2023-01-21 19:12:22 -0600

    Add 2015 days 17-18

---
+++ 2015/day17.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+'''
+https://adventofcode.com/2015/day/17
+'''
+import collections
+from collections.abc import Iterator
+
+# Local imports
+from aoc import AOC
+
+
+class AOC2015Day17(AOC):
+    '''
+    Day 17 of Advent of Code 2015
+    '''
+    day = 17
+
+    def __init__(self, example: bool = False) -> None:
+        '''
+        Load the instructions
+        '''
+        super().__init__(example=example)
+
+        self.target = 25 if self.example else 150
+        with self.input.open() as fh:
+            self.containers = dict(enumerate(int(line) for line in fh))
+
+    def enumerate(self, target: int | None = None) -> Iterator[tuple[int]]:
+        '''
+        Enumerates subsets of containers using bitwise AND.
+
+        Given a set S, the number of possible subsets is equal to (2^len(S))-1,
+        as shown below for a set of length 3:
+
+            1) a
+            2)   b
+            3) a b
+            4)     c
+            5) a   c
+            6)   b c
+            7) a b c
+
+        Subset membership for a given possible subset can thus be calculated
+        using bitwise AND, given the following:
+
+          1) Each possible subset can be represented as a binary representation
+             of a number from 1 to (2^len(S)-1), i.e. 001 to 111 in our example
+             set above, where a 1 means that position of the set is a member,
+             and a zero means that it is not.
+
+          2) Assigning each element of the set an index, that element is a
+             member of a given subset if its index is "turned on" in the
+             bitmask. For example, in combination 6 above the bitmask would be
+             "110" (position 0 is turned off, while positions 1 and 2 are
+             turned on). The set items "a", "b", "c" are assigned indexes 0, 1,
+             and 2, respectively. Thus, "b" and "c" are in this subset.
+
+        Mathematically, an element's membership can be calculated by performing
+        a bitwise AND between the mask and 2^i, where i is the index of the
+        element. If the result is zero, the element is not in the subset. Any
+        other result means that the element is in the subset. In our example
+        above for combination 6, we would determine set membership as follows:
+
+        Element 0: 110 AND 001 -> 000 (not in subset)
+        Element 1: 110 AND 010 -> 010 (in subset)
+        Element 2: 110 AND 100 -> 100 (in subset)
+
+        This function also has an optional argument "target", which will only
+        return combinations whose sum is equal to the target.
+        '''
+        for bitmask in range(1, 2**len(self.containers)):
+            subset = tuple(
+                volume for index, volume in self.containers.items()
+                if bitmask & 2**index
+            )
+            if target is None or sum(subset) == target:
+                yield subset
+
+    def part1(self) -> int:
+        '''
+        Calculate the number of combinations that add up to the target volume
+        '''
+        return sum(1 for subset in self.enumerate(target=self.target))
+
+    def part2(self) -> int:
+        '''
+        Calculate the number of unique combinations with the minimum number of
+        containers that add up to the target
+        '''
+        combo_map = collections.defaultdict(int)
+        for subset in self.enumerate(target=self.target):
+            combo_map[len(subset)] += 1
+
+        return combo_map[min(combo_map)]
+
+
+if __name__ == '__main__':
+    # Run against test data
+    aoc = AOC2015Day17(example=True)
+    aoc.validate(aoc.part1(), 4)
+    aoc.validate(aoc.part2(), 3)
+    # Run against actual data
+    aoc = AOC2015Day17(example=False)
+    aoc.run()
---
+++ 2015/day18.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python
+'''
+https://adventofcode.com/2015/day/18
+'''
+import itertools
+
+# Local imports
+from aoc import AOC
+
+# Typing shortcuts
+Coordinate = tuple[int, int]
+
+
+class AOC2015Day18(AOC):
+    '''
+    Day 18 of Advent of Code 2015
+    '''
+    day = 18
+
+    def animate(self, rounds: int, stuck_corners: bool = False) -> int:
+        '''
+        Animate the grid for the specified number of rounds, and returns the
+        number of lights which are lit after the specified number of rounds.
+        '''
+        # Load the initial state of the grid
+        grid = set()
+        with self.input.open() as fh:
+            for row, line in enumerate(fh):
+                for col, char in enumerate(line.rstrip()):
+                    if char == '#':
+                        grid.add((col, row))
+
+        # The grid is a square, get the size of one side
+        with self.input.open() as fh:
+            size = len(next(fh).rstrip())
+
+        corners = frozenset(itertools.product((0, size-1), repeat=2))
+        if stuck_corners:
+            # Make sure the corners are turned on
+            grid |= corners
+
+        def neighbors(coord: Coordinate) -> tuple[list[Coordinate]]:
+            '''
+            Return the neighboring coordinates which are within the grid. Two
+            sequences are returned, the ones which are lit, and the ones which
+            are not lit.
+            '''
+            lit = []
+            unlit = []
+
+            for delta in itertools.product((-1, 0, 1), repeat=2):
+                if delta == (0, 0):
+                    continue  # Not an actual neighbor, skip
+                neighbor = tuple(map(sum, zip(coord, delta)))
+                if any(not 0 <= index < size for index in neighbor):
+                    continue  # Out of bounds
+                if neighbor in grid:
+                    lit.append(neighbor)
+                else:
+                    unlit.append(neighbor)
+
+            return lit, unlit
+
+        for _ in range(rounds):
+            turn_off = set()
+            to_check = set()
+
+            for light in grid:
+                lit, unlit = neighbors(light)
+                to_check.update(unlit)
+                if stuck_corners and light in corners:
+                    continue
+                if len(lit) not in (2, 3):
+                    turn_off.add(light)
+
+            turn_on = {
+                coord for coord in to_check
+                if len(neighbors(coord)[0]) == 3
+            }
+
+            grid -= turn_off
+            grid |= turn_on
+
+        return len(grid)
+
+    def part1(self) -> int:
+        '''
+        Calculate the number of lights lit after the specified number of rounds
+        '''
+        return self.animate(rounds=4 if self.example else 100)
+
+    def part2(self) -> int:
+        '''
+        Calculate the number of lights lit after the specified number of
+        rounds, with the four corners stuck in the "on" position.
+        '''
+        return self.animate(
+            rounds=5 if self.example else 100,
+            stuck_corners=True,
+        )
+
+
+if __name__ == '__main__':
+    # Run against test data
+    aoc = AOC2015Day18(example=True)
+    aoc.validate(aoc.part1(), 4)
+    aoc.validate(aoc.part2(), 17)
+    # Run against actual data
+    aoc = AOC2015Day18(example=False)
+    aoc.run()
